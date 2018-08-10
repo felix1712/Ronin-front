5e6e1af9d9ccd68e4ca0ed77f76b01f733a966ad
lix <felix@talenta.co>
2018-08-10 14:38:40 +0700

    try implement api to home

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,7 +1,32 @@
 <template lag="pug" src="./home.pug"></template>
 
 <script>
+	import {service} from '@/api/main-service';
+
 	export default{
 		name:'Home',
+		data(){
+			return {
+				posts: [] 
+			}
+		},
+
+		methods: {
+			testApi(){
+				 service.get(`me`)
+			    .then(response => {
+			      this.posts = response.data
+			    })
+			    .catch(e => {
+			      this.errors.push(e)
+			    })
+			}
+		},
+		components: {
+
+		}
+
+
+		
 	};
 </script>
