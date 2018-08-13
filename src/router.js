80a96123b97c339484da98a385ae63e8e6767c3c
lix <felix@talenta.co>
2018-08-13 11:18:44 +0700

    add module create review

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/home/home.vue';
 import auth from './views/auth/auth.vue';
+import createReview from './views/createReview/createReview.vue';
 
 Vue.use(Router);
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/reviews/create-review',
+      name: 'reviews',
+      component: createReview,
     },
     {
       path: '/about',
---
+++ src/views/createReview/createReview.vue
@@ -0,0 +1,23 @@
+<template lag="pug" src="./index.pug"></template>
+
+<script>
+	import {service} from '@/api/main-service';
+
+	export default{
+		name:'createReview',
+		data(){
+			return {
+				posts: [] 
+			}
+		},
+
+		methods: {
+		},
+		components: {
+
+		}
+
+
+		
+	};
+</script>
