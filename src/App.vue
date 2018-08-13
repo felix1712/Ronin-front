7a4d3d9ca27d1d03af457c289f6012dbd94e900c
lix <felix@talenta.co>
2018-08-13 11:37:19 +0700

    div app fixing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
 	<div id="app" class="u-bg-snow">
 		<Header/>
-		<router-view/>
+		<div class="p-prfrm-wrapper">
+		  <div class="p-prfrm-content u-hsm-up u-tac u-py-90">
+		    <h1 class="c-smb-18 u-mb-40">
+		    	Halaman tidak tersedia dalam ukuran mobile.
+		    </h1>
+		    <!-- <img src=""> -->
+		  </div>
+			<router-view/>
+		</div>
 	</div>
 </template>
 
