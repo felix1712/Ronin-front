4ae88ee47b4774c3c38112cd3f11a361aa8df9a6
lix <felix@talenta.co>
2018-08-13 15:07:36 +0700

    change App into pug

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
-<template>
-	<div id="app" class="u-bg-snow">
-		<Header/>
-		<div class="p-prfrm-wrapper">
-		  <div class="p-prfrm-content u-hsm-up u-tac u-py-90">
-		    <h1 class="c-smb-18 u-mb-40">
-		    	Halaman tidak tersedia dalam ukuran mobile.
-		    </h1>
-		    <!-- <img src=""> -->
-		  </div>
-			<router-view/>
-		</div>
-	</div>
+<template lang="pug">
+	#app.u-bg-snow
+		Header
+		.p-prfrm-wrapper
+			.p-prfrm-content.u-hsm-up.u-tac.u-py-90
+				h1.c-smb-18.u-mb-40 Halaman tidak tersedia dalam ukuran mobile.
+				<!-- img(src="/assets/images/pages/icon-goals.svg", alt="") -->
+			router-view
+
+		<!-- Modal Section -->
+		.c-popover.jc-povc-confirmation.u-w-400.u-dn
+			.u-tac
+				h3.c-smb-15.u-mb-20 Cancel confirmation
+				p Are you sure want to cancel this process?
+				.u-mt-40
+					button.c-btn.c-btn--ghost.c-btn--sm.jc-pov-x No
+					button.c-btn.c-btn--delete.c-btn--sm(type="submit") Yes
 </template>
 
 <script>
