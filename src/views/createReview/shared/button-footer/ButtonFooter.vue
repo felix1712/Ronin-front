189348e635f4c6cf763cac4338efe2a9d2caab8d
lix <felix@talenta.co>
2018-08-14 11:01:34 +0700

    add dynamic button

---
+++ src/views/createReview/shared/button-footer/ButtonFooter.vue
@@ -0,0 +1,25 @@
+<template lang="pug" src="./index.pug"></template>
+
+<script>
+export default {
+  name: 'ButtonFooter',
+ 	data(){
+		return {
+			reviewStep: 1
+		}
+	},
+  methods: {
+  	prevReviewStep() {
+
+      // this.reviewStep--;
+    },
+    nextReviewStep() {
+      // this.reviewStep++;
+      debugger
+    },
+    submit() {
+      // alert('Submit to blah and show blah and etc.');
+    }
+	  },
+};
+</script>
