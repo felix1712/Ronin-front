8b9929ec7216d48a65e2ec6a1f0c7bd4bc52b407
lix <felix@talenta.co>
2018-08-24 11:21:16 +0700

    init buefy

--- src/main.js
+++ src/main.js
@@ -3,15 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VeeValidate from 'vee-validate';
-import VueToastr from '@deveodk/vue-toastr'
+import Buefy from 'buefy'
 // You need a specific loader for CSS files like https://github.com/webpack/css-loader
 // If you would like custom styling of the toastr the css file can be replaced
 import '@deveodk/vue-toastr/dist/@deveodk/vue-toastr.css'
 
-Vue.use(VueToastr, {
-    defaultPosition: 'toast-bottom-left',
-    defaultType: 'info',
-    defaultTimeout: 1000});
+Vue.use(Buefy)
 Vue.use(VeeValidate);
 Vue.use(require('vue-moment'));
 Vue.config.productionTip = false;
