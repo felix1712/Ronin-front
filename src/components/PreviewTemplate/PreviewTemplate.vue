bad83678553df7b97c175a1685b92e82c2942ec0
mulianaamulianaa <muliana@talenta.co>
2018-09-05 18:44:57 +0700

    add value radio button

--- src/components/PreviewTemplate/PreviewTemplate.vue
+++ src/components/PreviewTemplate/PreviewTemplate.vue
@@ -5,19 +5,19 @@ import BlankLayouts from '@/layouts/BlankLayouts/BlankLayouts.vue';
 import { service } from '@/api/main-service';
   
   export default{
-    name: 'PreviewTemplate',
-    data(){
-    	return {
-    		templatesUuid: null,
-    		template: "",
-        radioButton: 1,
-    	}
-    },
+	name: 'PreviewTemplate',
+	data(){
+		return {
+			templatesUuid: null,
+			template: "",
+			radioButton: "Yes",
+		}
+	},
 
-    created() {
-    	this.$emit(`update:layout`, BlankLayouts);
-    	this.templatesUuid = this.$route.params.uuid;
-    	service.get('templates/'+this.templatesUuid)
+	created() {
+		this.$emit(`update:layout`, BlankLayouts);
+		this.templatesUuid = this.$route.params.uuid;
+		service.get('templates/'+this.templatesUuid)
 			.then(response => {
 				this.template = response.data.contents.template;
 				// console.log(this.template);
@@ -25,12 +25,12 @@ import { service } from '@/api/main-service';
 			.catch(e =>{
 				console.log(e);
 			});
-    },
+	},
 
-    methods: {
-    	closeWindow(){
-    		window.close();
-    	}
-    }
+	methods: {
+		closeWindow(){
+			window.close();
+		}
+	}
   };
 </script>
