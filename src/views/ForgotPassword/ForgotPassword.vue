508044a57a70446fed494264eed5115ecfcab2ec
lix <felix@talenta.co>
2018-09-06 11:38:52 +0700

    forgot password content

--- src/views/ForgotPassword/ForgotPassword.vue
+++ src/views/ForgotPassword/ForgotPassword.vue
@@ -5,8 +5,20 @@ import LoginLayouts from '@/layouts/LoginLayouts/LoginLayouts.vue';
 
   export default {
     name: 'ForgotPassword',
+    data() {
+    	return {
+    		hasLink: false,
+    	}
+    },
+
     created() {
     	this.$emit(`update:layout`, LoginLayouts);
+    },
+
+    methods: {
+    	sendLinkForgot() {
+    		this.hasLink = true;
+    	}
     },
   };
 </script>
