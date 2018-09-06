17d013b491451b35b5bdadc9260925828a7a308d
lix <felix@talenta.co>
2018-09-06 11:20:23 +0700

    change position of services

--- src/api/main-service.js
+++ src/api/main-service.js
@@ -1,19 +1,6 @@
 import axios from 'axios';
 import Vue from 'vue';
 
-var services = new Vue()
-
-import VueCookie from 'vue-cookie';
-Vue.use(VueCookie);
-
 export const loginService = axios.create({
   baseURL: process.env.VUE_APP_OLD_API_URL,
-});
-
-
-export const service = axios.create({
-  baseURL: process.env.VUE_APP_OLD_API_URL,
-  headers: {
-    Authorization: services.$cookie.get('AuthPrfrm'),
-  },
 });
