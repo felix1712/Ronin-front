46c0d79257fca4266e55100ade85e9fcd9e7eec0
lix <felix@talenta.co>
2018-09-18 09:29:18 +0700

    list review unfixed

---
+++ src/components/CompanyReviewLists/CompanyReviewList.vue
@@ -0,0 +1,16 @@
+<template lang="pug" src="./index.pug"></template>
+
+<script>
+	export default{
+		name: 'CompanyReviewList',
+		props: {
+			listCompanyReview: {
+				type: Array
+			},
+			listCompanyIncluded: {
+				type: Array
+			}
+		},
+
+	};
+</script>
--- src/views/CompanyReviews/CompanyReview.vue
+++ src/views/CompanyReviews/CompanyReview.vue
@@ -2,16 +2,49 @@
 
 <script>
   import MainLayouts from '@/layouts/MainLayouts/MainLayouts.vue';
+  import axios from 'axios';
+  import CompanyReviewList from '@/components/CompanyReviewLists/CompanyReviewList.vue';
   export default{
     name:'CompanyReview',
     data() {
       return {
         activeTab: 0,
+        api: null,
+        listCompanyReview: null,
+        listCompanyIncluded: null,
       }
     },
 
     created (){
       this.$emit(`update:layout`, MainLayouts);
     },
+
+    mounted() {
+      const checkToken = this.$cookie.get('AuthToken');
+      const checkRefreshToken = this.$cookie.get('AuthRefresh');
+      if(checkToken != null && checkRefreshToken != null){
+        this.api = axios.create({
+          baseURL: process.env.VUE_APP_API,
+          headers: {
+            Authorization: this.$cookie.get('AuthToken'),
+            Refresh: this.$cookie.get('AuthRefresh'),
+          },
+        });
+
+        this.api.get('review/company-review',)
+        .then(response => {
+          this.listCompanyReview = response.data.data;
+          this.listCompanyIncluded = response.data.included;
+          console.log(response.data);
+        })
+        .catch(e => {
+          // this.errors.push(e);
+        });
+      }
+    },
+
+    components: {
+      CompanyReviewList,
+    }
   };
 </script>
