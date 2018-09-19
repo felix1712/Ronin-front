64c77a3cdb101ab73f8aa5ec56f3bd7bb87b1374
lix <felix@talenta.co>
2018-09-19 11:12:35 +0700

    break down user avatar list and top into component

---
+++ src/components/UserProfiles/UserProfileLists/UserProfileList.vue
@@ -0,0 +1,8 @@
+<template lang="pug" src="./index.pug"></template>
+
+<script>
+  export default{
+    name: 'UserProfileList',
+    props: ['avatarProfileList' ,'nameProfileList', 'jobTitleProfileList', 'departmentProfileList']
+  };
+</script>
