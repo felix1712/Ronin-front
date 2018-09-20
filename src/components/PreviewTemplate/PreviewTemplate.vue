ed2303bcbf5091ff9361382f3d2825823afffa7c
lix <felix@talenta.co>
2018-09-20 09:31:40 +0700

    fixing preview template and change router cancel in create review

--- src/components/PreviewTemplate/PreviewTemplate.vue
+++ src/components/PreviewTemplate/PreviewTemplate.vue
@@ -5,51 +5,52 @@ import BlankLayouts from '@/layouts/BlankLayouts/BlankLayouts.vue';
 import axios from 'axios';
   
   export default{
-	name: 'PreviewTemplate',
-	data(){
-		return {
-			api: null,
-			templateId: null,
-			template: "",
-			radioButton: "Yes",
-			isAccActive: true,
-		}
-	},
-
-	created() {
-		this.$emit(`update:layout`, BlankLayouts);
-		const checkToken = this.$cookie.get('AuthToken');
-		const checkRefreshToken = this.$cookie.get('AuthRefresh');
-		if(checkToken != null && checkRefreshToken != null){
-			this.api = axios.create({
-			  baseURL: process.env.VUE_APP_API,
-			  headers: {
-			    Authorization: this.$cookie.get('AuthToken'),
-				  Refresh: this.$cookie.get('AuthRefresh'),
-			  },
-			});
-
-			this.templateId = this.$route.params.id;
-
-			this.api.get('review/template/'+this.templateId)
-			.then(response => {
-				this.template = response.data.data.relationships.category_templates;
-				console.log(response);
-			})
-			.catch(e =>{
-				console.log(e);
-			});
-		}
-	},
-
-	methods: {
-		closeWindow(){
-			window.close();
+		name: 'PreviewTemplate',
+		data(){
+			return {
+				api: null,
+				templateId: null,
+				template: "",
+				radioButton: "Yes",
+				isAccActive: true,
+			}
 		},
 
-		accToggle(){
-			this.isAccActive = !this.isAccActive
-		}
-	}
+		created() {
+			this.$emit(`update:layout`, BlankLayouts);
+			const checkToken = this.$cookie.get('AuthToken');
+			const checkRefreshToken = this.$cookie.get('AuthRefresh');
+			if(checkToken != null && checkRefreshToken != null){
+				this.api = axios.create({
+				  baseURL: process.env.VUE_APP_API,
+				  headers: {
+				    Authorization: this.$cookie.get('AuthToken'),
+					  Refresh: this.$cookie.get('AuthRefresh'),
+				  },
+				});
+
+				this.templateId = this.$route.params.id;
+
+				this.api.get('review/template/'+this.templateId)
+				.then(response => {
+					this.template = this.$normalize.deserialize(response.data).category_templates.data;
+					console.log(this.template);
+					console.log(response)
+				})
+				.catch(e =>{
+					console.log(e);
+				});
+			}
+		},
+
+		methods: {
+			closeWindow(){
+				window.close();
+			},
+
+			accToggle(){
+				this.isAccActive = !this.isAccActive
+			}
+		},
   };
 </script>
